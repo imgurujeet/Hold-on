8498a699e577e3a4784c8703529552e2bf9a6cbe
imgurujeet <imgurujeet@gmail.com>
2026-01-26 21:59:53 +0530

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.silentchaos.holdon"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
